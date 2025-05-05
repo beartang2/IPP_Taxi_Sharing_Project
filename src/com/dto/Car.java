19782c83971e8c31223a8bb6bf0cbf07ad340d20
lanow0410 <jinsung1358@gmail.com>
2025-05-05 19:50:36 +0900

    testing

--- src/com/dto/Car.java
+++ src/com/dto/Car.java
@@ -1,5 +1,38 @@
 package com.dto;
 
 public class Car {
-
+	
+	private int carNumber;
+	private int seats;
+	private int capacity;
+	
+	public Car(int carNumber, int seats, int capacity) {
+		this.carNumber=carNumber;
+		this.seats=seats;
+		this.capacity=capacity;
+		
+	}
+	
+	
+	//setter getter
+	public int getCarNumber() {
+		return carNumber;
+	}
+	public void setCarNumber(int carNumber) {
+		this.carNumber = carNumber;
+	}
+	
+	public int getSeats() {
+		return seats;
+	}
+	public void setSeats(int seats) {
+		this.seats = seats;
+	}
+	
+	public int getCapacity() {
+		return capacity;
+	}
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
 }
