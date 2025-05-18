095c3fdbc9254e4ee5062d44649a3d6abd3de918
김준엽 <@LAPTOP-NP144UPE>
2025-05-18 13:28:09 +0900

    TheLowerCar

---
+++ taxi/src/main/java/dto/TheLowerCar.java
@@ -0,0 +1,21 @@
+public class TheLowerCar extends Car {
+	//상부공간
+    private boolean UpperSpace;
+
+    // 생성자
+    public TheLowerCar(int carNumber, int seats, int capacity, boolean UpperSpace) {
+        super(carNumber, seats, capacity);
+        this.UpperSpace = UpperSpace;
+    }
+
+    // getter setter
+    public boolean getUpperSpace() {
+        return UpperSpace;
+    }
+
+    public void setUpperSpace(boolean UpperSpace) {
+        this.UpperSpace = UpperSpace;
+    }
+
+  
+}
