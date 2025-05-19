83a874b2a1c37c08503d1f9b6299619852a152f5
Lenovo <Lenovo@DESKTOP-QIS1FUC>
2025-05-19 18:48:07 +0900

    2025.05.19

--- JAVA_Texi_Sharing_Project/src/com/dao/Reservation.java
+++ JAVA_Texi_Sharing_Project/src/com/dao/Reservation.java
@@ -1,25 +1,25 @@
 package com.dao;
 
+import com.dto.Car;
+import com.dto.User;
 import java.io.Serializable;
 
 public class Reservation implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    private int reservationId;       // 예약 고유 ID
-//    private User user;               // 예약자 정보
-//    private Car car;                 // 배정된 차량
-    private String departure;        // 출발지
-    private String destination;      // 목적지
-    private String time;             // 출발 시간 (예: "13:00")
-    private int hour;                // 출발 시각 정수 (FareCalc용)
-    private int people;              // 탑승 인원
-    private boolean hasLuggage;      // 짐 여부
-    private String carType;          // 차량 타입 (예: "Sedan", "SUV")
-    private int totalFare;           // 전체 요금
-    private int farePerPerson;       // 1인당 요금
+    private int reservationId;
+    private User user;
+    private Car car;
+    private String departure;
+    private String destination;
+    private String time;
+    private int hour;
+    private int people;
+    private boolean hasLuggage;
+    private String carType;
+    private int totalFare;
+    private int farePerPerson;
 
-    // 생성자
-    /*
     public Reservation(int reservationId, User user, Car car,
                        String departure, String destination,
                        String time, int hour, int people,
@@ -36,103 +36,44 @@ public class Reservation implements Serializable {
         this.hasLuggage = hasLuggage;
         this.carType = carType;
 
-        boolean isNight = com.dao.FareCalc.isNightTime(hour);
-        this.totalFare = com.dao.FareCalc.calcTotalFare(distance, isNight);
-        this.farePerPerson = com.dao.FareCalc.calcPerPersonFare(distance, people, isNight);
-    }
-*/
-    // Getter & Setter
-    public int getReservationId() {
-        return reservationId;
+        boolean isNight = FareCalc.isNightTime(hour);
+        this.totalFare = FareCalc.calcTotalFare(distance, isNight);
+        this.farePerPerson = FareCalc.calcPerPersonFare(distance, people, isNight);
     }
 
-    public void setReservationId(int reservationId) {
-        this.reservationId = reservationId;
-    }
+    public int getReservationId() { return reservationId; }
+    public void setReservationId(int reservationId) { this.reservationId = reservationId; }
 
-    /*
-    public User getUser() {
-        return user;
-    }
+    public User getUser() { return user; }
+    public void setUser(User user) { this.user = user; }
 
-    public void setUser(User user) {
-        this.user = user;
-    }
+    public Car getCar() { return car; }
+    public void setCar(Car car) { this.car = car; }
 
-    public Car getCar() {
-        return car;
-    }
+    public String getDeparture() { return departure; }
+    public void setDeparture(String departure) { this.departure = departure; }
 
-    public void setCar(Car car) {
-        this.car = car;
-    }
-     */
+    public String getDestination() { return destination; }
+    public void setDestination(String destination) { this.destination = destination; }
 
-    public String getDeparture() {
-        return departure;
-    }
+    public String getTime() { return time; }
+    public void setTime(String time) { this.time = time; }
 
-    public void setDeparture(String departure) {
-        this.departure = departure;
-    }
+    public int getHour() { return hour; }
+    public void setHour(int hour) { this.hour = hour; }
 
-    public String getDestination() {
-        return destination;
-    }
+    public int getPeople() { return people; }
+    public void setPeople(int people) { this.people = people; }
 
-    public void setDestination(String destination) {
-        this.destination = destination;
-    }
+    public boolean isHasLuggage() { return hasLuggage; }
+    public void setHasLuggage(boolean hasLuggage) { this.hasLuggage = hasLuggage; }
 
-    public String getTime() {
-        return time;
-    }
+    public String getCarType() { return carType; }
+    public void setCarType(String carType) { this.carType = carType; }
 
-    public void setTime(String time) {
-        this.time = time;
-    }
+    public int getTotalFare() { return totalFare; }
+    public int getFarePerPerson() { return farePerPerson; }
 
-    public int getHour() {
-        return hour;
-    }
-
-    public void setHour(int hour) {
-        this.hour = hour;
-    }
-
-    public int getPeople() {
-        return people;
-    }
-
-    public void setPeople(int people) {
-        this.people = people;
-    }
-
-    public boolean isHasLuggage() {
-        return hasLuggage;
-    }
-
-    public void setHasLuggage(boolean hasLuggage) {
-        this.hasLuggage = hasLuggage;
-    }
-
-    public String getCarType() {
-        return carType;
-    }
-
-    public void setCarType(String carType) {
-        this.carType = carType;
-    }
-
-    public int getTotalFare() {
-        return totalFare;
-    }
-
-    public int getFarePerPerson() {
-        return farePerPerson;
-    }
-
-    /*
     @Override
     public String toString() {
         return "Reservation{" +
@@ -150,5 +91,4 @@ public class Reservation implements Serializable {
                 ", farePerPerson=" + farePerPerson +
                 '}';
     }
-    */
 }
